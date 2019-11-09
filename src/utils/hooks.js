63075918fc312a9f76ddf51ceb193f990a67caf2
Parviz Khavari <csctclan@gmail.com>
2019-11-09 20:51:42 +0300

    Fix handling request error

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -5,11 +5,16 @@ export const useFetch = (method) => {
   const [loading, setLoading] = useState(true)
   useEffect(() => {
     async function fetchUrl () {
-      const response = await method()
-      const json = response.data
-      console.log(json)
-      setData(json)
-      setLoading(false)
+      try {
+        const response = await method()
+        const json = response.data
+        console.log(json)
+        setData(json)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setLoading(false)
+      }
     }
     fetchUrl()
   }, [method])
